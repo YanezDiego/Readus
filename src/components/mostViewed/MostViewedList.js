8c26f518a16efd40f5ba77510f6ce8f25d9b1689
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-09-30 14:24:22 -0400

    add todo comment for referance later

--- src/components/mostViewed/MostViewedList.js
+++ src/components/mostViewed/MostViewedList.js
@@ -5,6 +5,7 @@ export default class MostViewedList extends Component{
 
   renderMostViewed = () => {
     return this.props.mostViewed.map(article =>
+      // Remeber: Move bellow to Most Viewed in refactor stage
       <div key={article.asset_id}>
         <h2><a target='_blank' href={`${article.url}`}>{article.title}</a></h2>
         <p>{article.abstract}</p><span>Total Shares: {article.total_shares}</span>
