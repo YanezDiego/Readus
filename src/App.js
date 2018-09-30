90b849d329161e5f83a56edd9a02c48895016532
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-09-30 12:57:16 -0400

    renders MostViewedContainer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MostViewedContainer from './containers/MostViewedContainer'
+
+
+
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,9 +17,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome Readus</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <MostViewedContainer/>
       </div>
     );
   }
