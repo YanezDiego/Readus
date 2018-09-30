bb9baedcc1c495a703041099ad2b5c35286dd883
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-09-30 12:56:14 -0400

    fetch correct information

--- src/containers/MostViewedContainer.js
+++ src/containers/MostViewedContainer.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import MostViewedList from '../components/mostViewed/MostViewedList'
+
+const uri = `https://api.nytimes.com`
+
 
 
 
 class MostViewedContainer extends Component {
+  state = {
+    mostViewed: []
+  }
 
+   fetchMostViewed = () => {
+     fetch(`${uri}/svc/mostpopular/v2/mostshared/technology/30.json?
+    &api-key=f98593a095b44546bf4073744b540da0`)
+    .then(resp => resp.json())
+    .then(data => this.setState({mostViewed: data.results}))
+  }
 
-
+  render(){
+    return(
+      <div>
+        <button onClick={this.fetchMostViewed}>Click Me</button>
+        <MostViewedList mostViewed={this.state.mostViewed}/>
+      </div>
+    )
+  }
 }
+
 
 
 export default MostViewedContainer;
