3548a5b33042dd6a0a0c8d6216c954f0d1c648f5
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-01 18:46:16 -0400

    removed comment in line 9

--- src/components/mostViewed/MostViewedList.js
+++ src/components/mostViewed/MostViewedList.js
@@ -6,15 +6,14 @@ export default class MostViewedList extends Component{
 
   renderMostViewed = () => {
     return this.props.mostViewed.map(article =>
-      // Rember: Move bellow to Most Viewed in refactor stage
-      <MostViewed key={article.asset_id} article={article}/>
+        <MostViewed key={article.asset_id} article={article} />
     )
   };
 
   render(){
         return(
           <div>
-            <h1>Most Viewed Articles in the las 30 days!</h1>
+            <h1>Most Viewed Articles in the last 30 days!</h1>
             {this.renderMostViewed()}
           </div>
         )
