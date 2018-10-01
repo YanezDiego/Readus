a96b991e200e91b19f0b3f17fc4702c27a4eb427
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-01 18:45:50 -0400

    renders correct information from techviewlist component

--- src/components/techView/TechView.js
+++ src/components/techView/TechView.js
@@ -3,10 +3,15 @@ import React from 'react';
 
 
 const TechView = (props) => {
-  return(
-    <img src={`${props.tech.multimedia[0].url}`}
-    <h2><a href={`${props.tech.url}`}>{props.tech.title}</a></h2>
-    <p>{props.tech.abstract}<span>{props.tech.byline}</span></p>
+  let img = props.tech.multimedia.map(img => console.log(img['url'].split()))
 
+  return(
+    <div key={props.tech.idx}>
+      <h2><a href={`${props.tech.url}`}>{props.tech.title}</a></h2>
+      <p>{props.tech.abstract}</p>
+      <p>{props.tech.byline}</p>
+    </div>
   )
-}
+};
+
+export default TechView;
