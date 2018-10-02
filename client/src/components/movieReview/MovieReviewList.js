2960773267f78b044648d07bf0d891ee8f971a93
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-02 18:28:17 -0400

    abstracts moviereviewlist and uses moviereview component to render info

--- client/src/components/movieReview/MovieReviewList.js
+++ client/src/components/movieReview/MovieReviewList.js
@@ -1,1 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MovieReview from './MovieReview'
+
+class MovieReviewList extends Component {
+
+  renderMovies = () => {
+    return this.props.movieReview.map((movie, idx) =>
+    <MovieReview key={idx} movie={movie}/>)
+  };
+
+  render(){
+    return(
+      <div>
+        <h2>Recent Movie Reviews</h2>
+        {this.renderMovies()}
+      </div>
+    );
+  }
+
+};
+
+
+
+
+export default MovieReviewList;
