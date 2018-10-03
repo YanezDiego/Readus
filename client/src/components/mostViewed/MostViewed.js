94f7cc545b388a7ccf497d86a8885b0a0c45da4c
Diego Yanez <diegoyanez@MacBook-Pro-6.local>
2018-10-03 13:13:13 -0400

    img tag renders correct img size for most viewed

--- client/src/components/mostViewed/MostViewed.js
+++ client/src/components/mostViewed/MostViewed.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 
 const MostViewed = (props) =>{
-
     return(
       <div key={props.article.asset_id}>
+        <img src={`${props.article.media[0]["media-metadata"][2].url}`}/>
         <h2><a target='_blank' href={`${props.article.url}`}>{props.article.title}</a></h2>
         <p>{props.article.abstract}</p><span>Total Shares: {props.article.total_shares}</span>
       </div>
