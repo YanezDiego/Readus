ab6eb7794ea16d7a11828b80aa6ffbb003a13057
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-05 13:25:56 -0400

    img tag renders correct url

--- client/src/components/mostViewed/MostViewed.js
+++ client/src/components/mostViewed/MostViewed.js
@@ -4,8 +4,8 @@ import React from 'react';
 const MostViewed = (props) =>{
     return(
       <div key={props.article.asset_id}>
-        <img src={`${props.article.media[0]["media-metadata"][2].url}`}/>
         <h2><a target='_blank' href={`${props.article.url}`}>{props.article.title}</a></h2>
+        <img src={`${props.article.media[0]["media-metadata"][2].url}`}/>
         <p>{props.article.abstract}</p><span>Total Shares: {props.article.total_shares}</span>
       </div>
     )
