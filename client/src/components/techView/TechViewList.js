5b308c4ec27e48ef6f7397606d53351714aaeaf3
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-05 13:24:55 -0400

    organized code

--- client/src/components/techView/TechViewList.js
+++ client/src/components/techView/TechViewList.js
@@ -5,12 +5,14 @@ import TechView from './TechView'
 
 class TechViewList extends Component {
 
+
   renderTechStories = () => {
     return this.props.techStories.map((tech, idx) => <TechView key={idx} tech={tech}/>)
   };
 
 
   render(){
+
     return(
       <div>
         <h1>Tech Stories</h1>
