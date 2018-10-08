899b14fd3d6b5b8f1e37daf17a7121479d9ad765
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-08 12:41:38 -0400

    addTechStorieToApi moved to action with payload

--- client/src/actions/savedStoriesAction.js
+++ client/src/actions/savedStoriesAction.js
@@ -3,8 +3,22 @@ export const fetchSavedStories = () => {
     return fetch(`http://localhost:3001/techs`)
       .then(resp => resp.json())
       .then(stories => {
-        console.log(stories)
         dispatch({type: 'FETCH_SAVED_STORIES_SUCCESS', payload: stories})
       })
+  }
+}
+
+export const addTechStorieToApi = (body) => {
+  return (dispatch) => {
+    return fetch(`http://localhost:3001/techs`,{
+      method: 'POST',
+      body: JSON.stringify({tech: body}),
+      headers:{
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(storie => dispatch({type: 'ADD_STORIE', payload: storie}))
   }
 }
