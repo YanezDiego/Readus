a6a3f0aca4e9be8e43b0873cf69847a705ca2ad8
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-08 14:33:35 -0400

    renders summary and mpaa rating

---
+++ client/src/components/movieReview/MovieSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const MovieSummary = props => {
+  return(
+    <div>
+      <p>{props.movie.summary_short}</p>
+      <p>Rated: {props.movie.mpaa_rating}</p>
+    </div>
+  )
+}
+
+export default MovieSummary
