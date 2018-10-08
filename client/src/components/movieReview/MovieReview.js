f2b90a1a33b09610d4c67562b99662127c27ac5e
Diego Yanez <diegoyanez@MacBook-Pro-6.fios-router.home>
2018-10-08 14:39:03 -0400

    imports movie component and movie summary

--- client/src/components/movieReview/MovieReview.js
+++ client/src/components/movieReview/MovieReview.js
@@ -1,15 +1,18 @@
 import React from 'react'
-
+import MovieSummary from './MovieSummary'
+import MovieImg from './MovieImg'
 
 const MovieReview = (props) => {
   return(
     <div>
       <h2><a href={`${props.movie.link.url}`}>{props.movie.display_title}</a></h2>
       <p>{props.movie.headline}</p>
-      <img src={`${props.movie.multimedia.src}`}/>
-      <p>{props.movie.summary_short}</p>
+      <MovieImg movie={props.movie}/>
+      {/* <img src={`${props.movie.multimedia.src}`}/> */}
+      <MovieSummary movie={props.movie}/>
+      {/* <p>{props.movie.summary_short}</p>
       <p>Rated: {`${props.movie.mpaa_rating}`}</p>
-      <p>{props.movie.byline}</p>
+      <p>{props.movie.byline}</p> */}
     </div>
   )
 };
