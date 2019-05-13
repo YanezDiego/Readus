8d13f3d462eacfb32ac772a9e1fe75af60920c02
Diego Yanez <diegoyanez@MacBook-Pro-9.local>
2019-05-13 13:05:36 -0400

    remove img tag

--- client/src/components/mostViewed/MostViewed.js
+++ client/src/components/mostViewed/MostViewed.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 
-const MostViewed = (props) =>{
+
+
+const MostViewed = props => {
+
     return(
       <div key={props.article.asset_id}>
         <h2><a target='_blank' href={`${props.article.url}`}>{props.article.title}</a></h2>
-        <img src={`${props.article.media[0]["media-metadata"][2].url}`}/>
+        {/* {props["article"]["media"][0]["media-metadata"] ? <img src={`${props["article"]["media"][0]["media-metadata"][2]["url"]}`}/> : <img src={`${props["article"]["media"][0]["media-metadata"][0]["url"]}`}/>} */}
         <p>{props.article.abstract}</p><span>Total Shares: {props.article.total_shares}</span>
       </div>
     )
-};
-
+}
 
 export default MostViewed;
